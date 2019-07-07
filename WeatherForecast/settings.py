539413d83da3dbfc4566567b77221eeea8de49b7
StepanTita <stapan.tytarenko@nure.ua>
2019-07-07 21:27:31 +0300

    static fixed2

--- WeatherForecast/settings.py
+++ WeatherForecast/settings.py
@@ -16,7 +16,6 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -119,19 +118,18 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
-
-# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATIC_URL = 'ForecastApp/static'
+
+
+
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static'),
-    'ForecastApp/static'
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, '/static'),
+#     os.path.join(BASE_DIR, 'ForecastApp/static')
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
