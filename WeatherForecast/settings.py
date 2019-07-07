a5a5ed01f7d2eacace8e97033f5d4df8239262ce
StepanTita <stapan.tytarenko@nure.ua>
2019-07-07 17:02:18 +0300

    deploy2

--- WeatherForecast/settings.py
+++ WeatherForecast/settings.py
@@ -133,5 +133,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'ForecastApp/static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
